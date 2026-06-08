52a7ba30418699a9a00eef05f5591b94bb054cb1
Murtuzo <murtuzo.mamadziyoev_2028@ucentralasia.org>
2026-06-08 11:52:19 +0600

    Export ROI extraction helper

--- ocr_engine/__init__.py
+++ ocr_engine/__init__.py
@@ -3,9 +3,10 @@ from .ocr_handler import OCRHandler
 from .text_parser import TextParser
 from .merge import merge_ocr_blocks, merge_parsed_fields
 from .tajik_normalize import normalize_ocr_results, normalize_text
+from .roi_parser import extract_roi_fields
 
 __all__ = [
     'validate_image', 'preprocess_image', 'OCRHandler', 'TextParser',
     'merge_ocr_blocks', 'merge_parsed_fields',
-    'normalize_ocr_results', 'normalize_text',
+    'normalize_ocr_results', 'normalize_text', 'extract_roi_fields',
 ]
